89489b4e7a30ebb072b09adb554e951b6c818314
tall-horse <54848630+tall-horse@users.noreply.github.com>
2023-08-24 13:08:07 +0200

    NetworkVariable syncronization

--- UnityNetcodeTest/Assets/Scripts/PlayerNetwork.cs
+++ UnityNetcodeTest/Assets/Scripts/PlayerNetwork.cs
@@ -1,15 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+  private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+    new MyCustomData
+    {
+      _int = 56,
+      _bool = true
+    },
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+  public struct MyCustomData : INetworkSerializable
+  {
+    public int _int;
+    public bool _bool;
+    public FixedString128Bytes message;
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+      serializer.SerializeValue(ref _int);
+      serializer.SerializeValue(ref _bool);
+      serializer.SerializeValue(ref message);
+    }
+  }
+  public override void OnNetworkSpawn()
+  {
+    randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+    {
+      Debug.Log(OwnerClientId + ";  " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+    };
+  }
   // Update is called once per frame
   void Update()
   {
+
     if (!IsOwner)
       return;
+
+    if (Input.GetKeyDown(KeyCode.T))
+    {
+      randomNumber.Value = new MyCustomData
+      {
+        _int = 10,
+        _bool = false,
+        message = "Test message",
+      };
+    }
+
     Vector3 moveDir = new Vector3(0, 0, 0);
     if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
     if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
